0f78c0e6c0c2b94c7fb4bb1cdce752a5ce79eae2
Eduardo Miquilena <emiquilena@aluxion.com>
2023-01-13 10:53:37 -0400

    fix:  name

--- daos/productos/ProductosDaoMongoDb.js
+++ daos/productos/ProductosDaoMongoDb.js
@@ -1,4 +1,4 @@
-import ContenedorMongoDb from "../../contenedores/contenedorMongoDb.js";
+import ContenedorMongoDb from "../../contenedores/ContenedorMongoDb.js";
 
 class ProductosDaoMongoDb extends ContenedorMongoDb {
   constructor() {
